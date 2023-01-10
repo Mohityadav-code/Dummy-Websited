88b61672856471daab0709cad20ca9a21ff1e8db
Mohityadav-code <mohityadav3552@gmail.com>
2023-01-10 10:28:11 +0530

    Created Script For Toggle in Menu Section

---
+++ Youtube/Script.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+
+menuIcon.onclick = function () {
+  sidebar.classList.toggle("small-sidebar");
+};
